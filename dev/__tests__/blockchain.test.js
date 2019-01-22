d8dbb40cb6d56558f037866b452417f0addcb0f6
waraphon roonnapai <eepw0211@gmail.com>
2019-01-22 15:29:46 +0700

    add proofOfWork code to blockchain

--- dev/__tests__/blockchain.test.js
+++ dev/__tests__/blockchain.test.js
@@ -1,14 +1,28 @@
 const Blockchain = require('../blockchain')
 const Bitcoin = new Blockchain()
 
-Bitcoin.createNewBlock(1255, 'jsdakldja12331sd7897', 'dasda121888')
-Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
-Bitcoin.createNewBlock(1256, 'jsdakldasd7897', 'dasdadasds8888')
-
-Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
-Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
-Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
-Bitcoin.createNewBlock(1256, 'jsd123dja8887d7897', 'dasd121238')
-
-console.log(Bitcoin.createNewTransaction(100, 'asdasd', 'asdad'))
 console.log(Bitcoin)
+
+// test proof of work
+// ================================
+// const previousBlockHash = 'asdasdasd'
+// const currentBlockData = [ {
+//   amount: 100, sender: 'aa', recipient: 'bb'
+// }, {
+//   amount: 300, sender: 'aa', recipient: 'bb'
+// }]
+
+// console.log(Bitcoin.proofOfWork(previousBlockHash, currentBlockData))
+// console.log(Bitcoin.hashBlock(previousBlockHash, currentBlockData, 46498))
+
+// test transaction and new block
+// ===============================
+// Bitcoin.createNewBlock(1255, 'jsdakldja12331sd7897', 'dasda121888')
+// Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
+// Bitcoin.createNewBlock(1256, 'jsdakldasd7897', 'dasdadasds8888')
+
+// Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
+// Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
+// Bitcoin.createNewTransaction(100, 'asdasd', 'asdad')
+// Bitcoin.createNewBlock(1256, 'jsd123dja8887d7897', 'dasd121238')
+// console.log(Bitcoin)
--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require('sha256')
+
 function Blockchain () {
   this.chain = []
   this.pendingTransaction = []
+
+  this.createNewBlock(100, '0', '0')
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, has) {
@@ -29,6 +33,24 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
 
   this.pendingTransaction = [...this.pendingTransaction, newTransaction]
   return this.getLastBlock()['index'] + 1
+}
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+  const dataAsString = `${previousBlockHash}${nonce.toString()}${JSON.stringify(currentBlockData)}`
+  const hash = sha256(dataAsString)
+
+  return hash
+}
+
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+  let nonce = 0
+  let hash = ''
+  while (hash.substring(0, 4) !== '0000') {
+    nonce++
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+  }
+
+  return nonce
 }
 
 module.exports = Blockchain
