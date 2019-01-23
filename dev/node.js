ddc1cbe961b9e9e644bdc48b34198c20e7116ae2
Waraphon Roonnapai <eepw0211@gmail.com>
2019-01-23 23:41:21 +0700

    build register-broadcast-node

--- dev/node.js
+++ dev/node.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain')
 const uuid = require('uuid')
 const port = process.argv[2] || 3000
+const rp = require('request-promise')
 
 const app = express()
 const bitcoin = new Blockchain()
@@ -43,12 +44,47 @@ app.get('/mine', (req, res) => {
 
 // register a node and broadcast it in the network
 app.post('/register-broadcast-node', (req, res) => {
-
+  const { newNodeUrl } = req.body
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) === -1) {
+    bitcoin.networkNodes = [...bitcoin.networkNodes, newNodeUrl]
+  }
+  let regNodePromise = []
+  bitcoin.networkNodes.map(networkNodeUrl => {
+    const requestOption = {
+      url: `${networkNodeUrl}/refister-node`,
+      method: 'POST',
+      body: { newNodeUrl: newNodeUrl },
+      json: true
+    }
+    regNodePromise = [...regNodePromise, rp(requestOption)]
+  })
+  Promise.all(regNodePromise).then(data => {
+    const bulkRegisterOption = {
+      url: `${newNodeUrl}/register-node-bulk`,
+      method: 'POST',
+      body: { allnetworknode: [...bitcoin.networkNodes, bitcoin.currentNodeUrl] },
+      json: true
+    }
+    return rp(bulkRegisterOption)
+  }).then(data => {
+    res.json({ message: 'New node registered with network successfully.' })
+  }).catch(error => {
+    console.error('/register-and-broadcast-node Promise error ' + error)
+    res.send(error)
+  })
 })
 
 // register a node with network
 app.post('/register-node', (req, res) => {
-
+  const { newNodeUrl } = req.body
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
+  const notAlreadyUrl = bitcoin.networkNodes.indexOf(newNodeUrl) === -1
+  if (notCurrentNode && notAlreadyUrl) {
+    bitcoin.networkNodes = [...bitcoin.networkNodes, newNodeUrl]
+  } else {
+    res.send({ message: 'New node already in network.' })
+  }
+  res.send({ message: 'New node registered successfully with node.' })
 })
 
 // register multiple nides at one
